12dc446e487f7382515a90e13eab51bbbcc1d7ef
KevKalazh <86112647+KevKalazh@users.noreply.github.com>
2021-07-16 14:20:11 +0200

    FrostWar Launcher

---
+++ Warriors Glorry/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿
+namespace Warriors_Glorry
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Beenden = new System.Windows.Forms.Button();
+            this.Play = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.Minimize = new System.Windows.Forms.Button();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Beenden
+            // 
+            this.Beenden.BackColor = System.Drawing.Color.Transparent;
+            this.Beenden.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.Beenden.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Beenden.Font = new System.Drawing.Font("The Wild Breath of Zelda", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Beenden.ForeColor = System.Drawing.Color.Transparent;
+            this.Beenden.Location = new System.Drawing.Point(1115, 5);
+            this.Beenden.Name = "Beenden";
+            this.Beenden.Size = new System.Drawing.Size(25, 21);
+            this.Beenden.TabIndex = 0;
+            this.Beenden.UseVisualStyleBackColor = false;
+            this.Beenden.Click += new System.EventHandler(this.Beenden_Click);
+            // 
+            // Play
+            // 
+            this.Play.BackColor = System.Drawing.Color.Transparent;
+            this.Play.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Play.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Play.Font = new System.Drawing.Font("The Wild Breath of Zelda", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Play.ForeColor = System.Drawing.Color.Transparent;
+            this.Play.Location = new System.Drawing.Point(979, 613);
+            this.Play.Name = "Play";
+            this.Play.Size = new System.Drawing.Size(132, 75);
+            this.Play.TabIndex = 1;
+            this.Play.UseVisualStyleBackColor = false;
+            this.Play.Click += new System.EventHandler(this.Play_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(10, 10);
+            this.contextMenuStrip1.LayoutStyle = System.Windows.Forms.ToolStripLayoutStyle.HorizontalStackWithOverflow;
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // Minimize
+            // 
+            this.Minimize.BackColor = System.Drawing.Color.Transparent;
+            this.Minimize.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.Minimize.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Minimize.Font = new System.Drawing.Font("The Wild Breath of Zelda", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Minimize.ForeColor = System.Drawing.Color.Transparent;
+            this.Minimize.Location = new System.Drawing.Point(1082, 6);
+            this.Minimize.Name = "Minimize";
+            this.Minimize.Size = new System.Drawing.Size(25, 20);
+            this.Minimize.TabIndex = 2;
+            this.Minimize.UseVisualStyleBackColor = false;
+            this.Minimize.Click += new System.EventHandler(this.Minimize_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.CausesValidation = false;
+            this.webBrowser1.ContextMenuStrip = this.contextMenuStrip1;
+            this.webBrowser1.IsWebBrowserContextMenuEnabled = false;
+            this.webBrowser1.Location = new System.Drawing.Point(59, 163);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.ScriptErrorsSuppressed = true;
+            this.webBrowser1.Size = new System.Drawing.Size(596, 399);
+            this.webBrowser1.TabIndex = 0;
+            this.webBrowser1.TabStop = false;
+            this.webBrowser1.Url = new System.Uri("http://frostwar.de/en/", System.UriKind.Absolute);
+            this.webBrowser1.WebBrowserShortcutsEnabled = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("The Wild Breath of Zelda", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Transparent;
+            this.button1.Location = new System.Drawing.Point(429, 658);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(205, 41);
+            this.button1.TabIndex = 4;
+            this.button1.UseVisualStyleBackColor = false;
+
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Font = new System.Drawing.Font("The Wild Breath of Zelda", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.Transparent;
+            this.button2.Location = new System.Drawing.Point(23, 664);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(126, 32);
+            this.button2.TabIndex = 5;
+            this.button2.UseVisualStyleBackColor = false;
+
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(1150, 720);
+            this.ControlBox = false;
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.Minimize);
+            this.Controls.Add(this.Beenden);
+            this.Controls.Add(this.Play);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Button Play;
+        private System.Windows.Forms.Button Beenden;
+        private System.Windows.Forms.Button Minimize;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Warriors Glorry/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace Warriors_Glorry
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Beenden_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Play_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(@"WoW.exe");
+            Application.Exit();
+           Play.FlatStyle = FlatStyle.Flat;
+            Play.FlatAppearance.BorderColor = BackColor;
+            Play.FlatAppearance.MouseOverBackColor = BackColor;
+            Play.FlatAppearance.MouseDownBackColor = BackColor;
+        }
+
+
+        protected override void WndProc(ref Message m)
+        {
+            switch (m.Msg)
+            {
+                case 0x84:
+                    base.WndProc(ref m);
+                    if ((int)m.Result == 0x1)
+                        m.Result = (IntPtr)0x2;
+                    return;
+            }
+
+            base.WndProc(ref m);
+        }
+
+
+
+        private void Minimize_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            webBrowser1.ScriptErrorsSuppressed = true;
+            
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://frostwar.de/en/");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void stop(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            System.IO.DirectoryInfo dir = new System.IO.DirectoryInfo(@"Cache");
+            if (dir.Exists)
+                dir.Delete(true);
+
+
+            string myfile = @"data/enUS/realmlist.wtf";
+
+
+            File.Delete(myfile);
+
+            if (!File.Exists(myfile))
+            {
+              
+                using (StreamWriter sw = File.CreateText(myfile))
+                {
+                    sw.WriteLine("set realmlist FrostWar.de");
+                }
+
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://frostwar.de/en/forum");
+        }
+
+
+    }
+}
